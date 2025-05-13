f2a159eac93221387afcfc01020b31423a66080a
이유진 <jiny3516@naver.com>
2025-05-13 10:05:15 +0900

    상단바 추가

--- UniLife/src/main/java/inhatc/hja/unilife/calendar/controller/AuthController.java
+++ UniLife/src/main/java/inhatc/hja/unilife/calendar/controller/AuthController.java
@@ -8,6 +8,6 @@ public class AuthController {
 
     @GetMapping("/login")
     public String loginForm() {
-        return "calendar/login"; // login.html 템플릿 이름 (src/main/resources/templates/login.html 있어야 함)
+        return "common/login";
     }
 }
