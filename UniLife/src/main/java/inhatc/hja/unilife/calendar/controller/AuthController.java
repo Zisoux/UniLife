d54aff5a380c8a3fd4a0ba685442f8f96d453f6f
hjs-gran <202345015@itc.ac.kr>
2025-05-21 16:05:44 +0900

    회원가입 기능 추가 진행 중 1차

--- UniLife/src/main/java/inhatc/hja/unilife/calendar/controller/AuthController.java
+++ UniLife/src/main/java/inhatc/hja/unilife/calendar/controller/AuthController.java
@@ -2,6 +2,9 @@ package inhatc.hja.unilife.calendar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class AuthController {
@@ -10,4 +13,13 @@ public class AuthController {
     public String loginForm() {
         return "common/login";
     }
+    
+    @GetMapping("/signup")
+    public String signupForm() {
+        return "common/signup";
+    }
+
+    //@PostMapping("/signup")
+    // 회원가입 기능 추가 예정
+    
 }
